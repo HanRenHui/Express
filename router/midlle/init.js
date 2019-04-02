7ae89afc8b2bac69eab268233beb7bf3fcec5143
HanRenHui <897691781@qq.com>
2019-04-02 22:48:19 +0800

    内置中间件

--- router/midlle/init.js
+++ router/midlle/init.js
@@ -3,5 +3,10 @@ module.exports = function (req, res, next) {
   let {pathname, query} = url.parse(req.url, true)
   req.path = pathname 
   req.query = query
+  res.json = function (obj) {
+    res.setHeader('content-type', 'application/json')
+    let str = JSON.stringify(obj)
+    res.end(str)
+  }
   next()
 }
